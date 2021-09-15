dbe7699ead004af5da8b9835fbfebe9d26a881b8
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-09-15 13:02:42 -0300

    Initial of MVP without random

--- index.js
+++ index.js
@@ -1,48 +1,84 @@
 let mentors = [{
     name: 'Juliano',
     class: 'Mentor',
-    day: 3,
-    hour: 2,
+    slot: 2,
 },
 {
     name: 'Joaquim',
     class: 'Mentor',
-    day: 1,
-    hour: 2,
+    slot: 3,
+},
+{
+    name: 'Giovani',
+    class: 'Mentor',
+    slot: 2,
+},
+{
+    name: 'Daniel',
+    class: 'Mentor',
+    slot: 1,
 },
 {
     name: 'Alex',
     class: 'Mentor',
-    day: 2,
-    hour: 2,
-},
-{
-    name: 'Alex',
-    class: 'Mentor',
-    day: 1,
-    hour: 1,
-},
-{
-    name: 'Alex',
-    class: 'Mentor',
-    day: 2,
-    hour: 1,
+    slot: 1,
 }];
+
+
 
 let leads = [{
-    name: 'Alex',
+    name: 'Geralda',
     class: 'Lead',
-    day: 'Ter',
-    hour: 'Manhã',
-}];
+    slot: 2,
+},
+{
+    name: 'Fabio',
+    class: 'Lead',
+    slot: 1,
+},
+{
+    name: 'Sabrina',
+    class: 'Lead',
+    slot: 3,
+},
+{
+    name: 'Julia',
+    class: 'Lead',
+    slot: 2,
+},
+{
+    name: 'Jaqueline',
+    class: 'Lead',
+    slot: 3,
+},];
 
 mentors.sort(function (x, y){
-    return x.day - y.day;
+    return x.slot - y.slot;
 });
 
-
+leads.sort(function (x, y){
+    return x.slot - y.slot;
+});
 
 console.log(mentors);
+console.log(leads);
+
+
+function pairs(){
+    let dobles = [];
+    for (let i = 0; i < mentors.length; i++){
+        console.log(mentors[i].slot);
+        console.log(leads[i].slot);
+        if (mentors[i].slot == leads[i].slot){
+            let pair = `Dupla: ${mentors[i].name} e ${leads[i].name}`
+            dobles = pair;
+            console.log(dobles)
+        }
+    }
+    return dobles
+}
+
+pairs();
 
 
 // function pairs(){
